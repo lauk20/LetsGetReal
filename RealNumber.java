d16e845f3ac7957c3e664b744d0b34736f642110
lauk20 <klau20@stuy.edu>
2020-11-20 12:14:22 -0500

    Added constructor for RealNumber and added getValue(). Added a test for it.

---
+++ RealNumber.java
@@ -0,0 +1,11 @@
+public class RealNumber{
+  private double value;
+
+  public RealNumber(double v){
+    value = v;
+  }
+
+  public double getValue(){
+    return value;
+  }
+}
---
+++ Tester.java
@@ -0,0 +1,8 @@
+public class Tester{
+  public static void main(String[] args){
+    RealNumber A = new RealNumber(100);
+    RealNumber B = new RealNumber(-0);
+    System.out.println(A.getValue());
+    System.out.println(B.getValue());
+  }
+}
