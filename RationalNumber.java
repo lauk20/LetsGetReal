01076aa02d10ca5198154ae041f9a01481c80b27
lauk20 <klau20@stuy.edu>
2020-11-21 15:19:55 -0500

    Fixed an error with finding the GCD in gcd() method for RationalNumber class.

--- RationalNumber.java
+++ RationalNumber.java
@@ -50,6 +50,9 @@ public class RationalNumber extends RealNumber{
   }
 
   private static int gcd(int a, int b){
+    a = Math.abs(a);
+    b = Math.abs(b);
+    
     if (a < b){
       int holder = b;
       b = a;
